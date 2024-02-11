25efb59ae2a5e48e6360fe71e85701f96a8f998f
cca medurir2 <medurir2@udayton.edu>
2024-02-11 17:21:26 -0500

    server

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use(cors({
-  origin: ['https://ramkiranmeduri.vercel.app'], // or use '*' to allow all origins
+  origin: ['https://ramkiranmeduri.vercel.app/'], // or use '*' to allow all origins
 methods:["POST","GET"],
 
 credentials: true}));
