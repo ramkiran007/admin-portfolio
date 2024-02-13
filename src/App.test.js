6b383763a6aba5a3eea3545334ec441ff0e613d8
cca medurir2 <medurir2@udayton.edu>
2024-02-12 22:32:46 -0500

    some changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 test('renders App component', () => {
-  render(<App />);
+    render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
