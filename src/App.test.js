f85d20df43f77adfaa953ee037366e2c697c8e80
cca medurir2 <medurir2@udayton.edu>
2024-02-12 23:01:44 -0500

    some changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,19 @@
+jest.mock('lottie-web', () => ({
+    loadAnimation: jest.fn(),
+  }));
+  import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-test('renders App component', () => {
+test('renders App component', async() => {
     render(
         <MemoryRouter>
           <App />
         </MemoryRouter>
       );
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
