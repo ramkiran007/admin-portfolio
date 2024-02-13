679f81c49c1f8752436f0e279f408d31edd0127b
cca medurir2 <medurir2@udayton.edu>
2024-02-12 22:21:08 -0500

    some changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import PortfolioManager from './App'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <PortfolioManager  />
+    <App  />
 
     </BrowserRouter>
 
