89806fdae3cf5090d92ececa3fe87da6d27291c4
Ram Kiran <medurir2@udayton.edu>
2024-02-18 09:23:00 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-const port = process.env.PORT ||3005
+const port = process.env.PORT ||8080
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
