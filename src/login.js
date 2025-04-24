6295f837f78fc4a7a1fb1b18d38c4ea8dfbc794d
Ram Kiran <medurir2@udayton.edu>
2025-04-24 18:13:08 -0400

    Update login.js

--- src/login.js
+++ src/login.js
@@ -3,6 +3,8 @@ import './App.css';
 import profiles from './uploads/pexels-puwadon-sang-ngern-2168173-5340280.jpg'
 import icon from './uploads/upload.png'
 import pdf from './uploads/CancerDiagnosis.pdf'
+import pdf2 from './uploads/Airline Analysis.pdf'
+
 const PortfolioCard = () => {
   return (
     <div className="card">
@@ -18,6 +20,8 @@ const PortfolioCard = () => {
   </a>
 </div>
 
+
+
        
       </div>
       <div className="carder-content">
@@ -28,6 +32,34 @@ const PortfolioCard = () => {
         
       </div>
     </div>
+
+<div className="card">
+      <div className="carder-image-container">
+        <img
+          src={profiles}
+          alt="project_image"
+          className="card-image"
+        />
+<div className="card-hover">
+  <a href={pdf2} target="_blank" rel="noopener noreferrer" style={{ display: 'block', width: '100%', height: '100%' }}>
+    {/* Additional content can go here if needed */}
+  </a>
+</div>
+
+
+
+       
+      </div>
+      <div className="carder-content">
+        <p className="carder-title">Cancer diagnosis</p>
+        <p className="card-description">
+
+This report analyzes domestic flight data from Q1 2019 to identify the best round-trip
+routes for a new airline entering the US market. The company plans to launch 5 routes
+using new airplanes that cost $90 million each, with a brand promise of "On time, for you."       
+      </div>
+    </div>
+
   );
 };
 
